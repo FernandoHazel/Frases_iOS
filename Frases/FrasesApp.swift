46a31470a71108c73f6d99c333da066e8acc7baa
Fernando Ascencio <fernando.ascencio@bradesco.com.mx>
2026-06-22 13:42:40 -0600

    Initial Commit

---
+++ Frases/FrasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrasesApp.swift
+//  Frases
+//
+//  Created by Fernando Hazel Ascencio Baumgarten on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FrasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrasesTests/FrasesTests.swift
@@ -0,0 +1,19 @@
+//
+//  FrasesTests.swift
+//  FrasesTests
+//
+//  Created by Fernando Hazel Ascencio Baumgarten on 22/06/26.
+//
+
+import Testing
+@testable import Frases
+
+struct FrasesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
